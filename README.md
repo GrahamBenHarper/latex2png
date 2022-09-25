b7bc3d3a0fc40d2c76264acbd40b797059819d32
Graham Harper <grahambenharper@gmail.com>
2022-09-24 21:47:17 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# latex2png
+A simple Flask-based application for rendering LaTeX equations as `.png` images.
+
+### Requirements
+This requires the python package Flask to run the application. It additionally requires texlive-latex-base, and imagemagick. Specifically, the commands `pdflatex` and `convert` from the latter two packages should be available in your `$PATH`. This has been tested on Ubuntu 22.04.
+
+### Running the application
+The application is run by running `app.py`. Then, the content should be visible on `127.0.0.1:5000` in any web browser. Simply enter an equation into the text field and click the submit button.
+
+### Troubleshooting
+When converting pdf files to images, you may see the following error:
+```
+convert-im6.q16: attempt to perform an operation not allowed by the security policy `PDF' @ error/constitute.c/IsCoderAuthorized/421.
+convert-im6.q16: no images defined `equation.png' @ error/convert.c/ConvertImageCommand/3229.
+```
+
+To fix this, simply modify ImageMagick's `policy.xml` file (located in `/etc/ImageMagick-6/policy.xml` on Linux-based systems)
+```
+  <policy domain="coder" rights="none" pattern="PDF" />
+```
+and add comment braces around the above line so it reads as
+```
+  <!-- <policy domain="coder" rights="none" pattern="PDF" /> -->
+```
---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, send_file
+from subprocess import Popen, PIPE, STDOUT, TimeoutExpired
+
+def RunCMD(cmd: str, timeout = 15):
+    """
+        Runs a command, captures stdout & stderr, trims output
+        timeout: how long to let command run, -1 for infinite
+    """
+
+    proc = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+
+    try:
+        if timeout == -1:
+            outs, errs = proc.communicate()
+        else:
+            outs, errs = proc.communicate(timeout=timeout)
+    except TimeoutExpired:
+        proc.kill()
+        outs, errs = proc.communicate()
+
+    outs = outs.decode("UTF-8").strip()
+    errs = (errs.decode("UTF-8").strip() if errs else None)
+
+    return {"out": outs, "err": errs, "ret": proc.returncode}
+
+
+app = Flask(__name__)
+
+
+def save_file():
+    # steps:
+    # 1. Create a file using the template
+    f = open("equation.tex","w")
+    f.write("\\documentclass[12pt]{article}\n")
+    f.write("\\usepackage[utf8]{inputenc}\n")
+    f.write("\\usepackage{amsmath,amsfonts,amssymb}\n")
+    f.write("\\begin{document}\n")
+    f.write("\\begin{align*}\n")
+    
+    equation = request.args.get("equation")
+    f.write(equation)
+    f.write("\\end{align*}\n")
+    f.write("\\pagenumbering{gobble}\n")
+    f.write("\\end{document}\n")
+    f.close()
+
+    # 2. Compile to pdf
+    cmd = "pdflatex equation.tex"
+    print("Executing: " + cmd)
+    res = RunCMD(cmd, -1)
+    # TODO: handle the command failing
+    #print("Command exited with status: " + res)
+    
+    # 3. Convert to png
+    cmd = "convert -density 300 equation.pdf -quality 90 -trim static/equation.png"
+    print("Executing: " + cmd)
+    res = RunCMD(cmd, -1)
+    # TODO: handle the command failing
+    #print("Command exited with status: " + res)
+    
+    return render_template("index.html", equation_image="equation.png")
+
+@app.route('/')
+def index():
+    if not request.args.get("equation"):
+        return render_template("index.html", equation_image="equation.png")
+
+    return save_file()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
